b12287309625d2128a9547beff97e39a9e5e3b34
Rirog <vitalijmaskov5135@gmail.com>
2025-12-18 18:32:21 +0300

    Смотри все тесты проверь по правильности

--- test_admin_user.py
+++ test_admin_user.py
@@ -30,19 +30,22 @@ class AdminUsersTests(unittest.TestCase):
 
         self.assertIn(response.status_code, [200, 403])
 
-    def test_get_all_users_info_with_params(self):
-        """Тест получения информации о пользователях с параметрами"""
-        url = f"{BASE_URL}/admin/list/users"
-        params = {"username": "Dupling1", "email": "Dupling1@example.com", "isBanned": False}
-        response = requests.get(url, headers=self.user_headers, params=params)
-        self.assertIn(response.status_code, [200, 403])
 
-    def test_get_all_banned_users(self):
-        """Тест получения информации о забаненных пользователях"""
-        url = f"{BASE_URL}/admin/list/users"
-        params = {"isBanned": True}
-        response = requests.get(url, headers=self.user_headers, params=params)
-        self.assertIn(response.status_code, [200, 403])
+    # я хуй знает типа что то одно тестим или как блять 
+    
+    # def test_get_all_users_info_with_params(self):
+    #     """Тест получения информации о пользователях с параметрами"""
+    #     url = f"{BASE_URL}/admin/list/users"
+    #     params = {"username": "Dupling1", "email": "Dupling1@example.com", "isBanned": False}
+    #     response = requests.get(url, headers=self.user_headers, params=params)
+    #     self.assertIn(response.status_code, [200, 403])
+
+    # def test_get_all_banned_users(self):
+    #     """Тест получения информации о забаненных пользователях"""
+    #     url = f"{BASE_URL}/admin/list/users"
+    #     params = {"isBanned": True}
+    #     response = requests.get(url, headers=self.user_headers, params=params)
+    #     self.assertIn(response.status_code, [200, 403])
 
     def test_get_user_info_by_id(self):
         """Тест получения информации о пользователе по ID"""
